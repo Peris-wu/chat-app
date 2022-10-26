ca8fd63918c9d8a1ce6741e2987c306c8784156c
peris <964480899@qq.com>
2022-10-26 23:10:35 +0800

    用户详情页

---
+++ components/editorinfo/editorinfo.vue
@@ -0,0 +1,97 @@
+<template>
+	<view class="editor">
+		<view class="header">
+			<chat-header>
+				<template v-slot:left>
+					<slot name="left">
+						<view @tap="cancel">
+							取消
+						</view>
+					</slot>
+				</template>
+				<template v-slot:middle>
+					<slot name="middle">
+						编辑信息
+					</slot>
+				</template>
+				
+				<template v-slot:right>
+					<view class="right" @tap="achivement">
+						<slot name="right">
+							确定
+						</slot>
+					</view>
+				</template>
+			</chat-header>
+		</view>
+		<view class="edit-area">
+			<view v-if="editorType === 'textarea'">
+				<textarea v-model="showMsg" :value="showMsg" maxlength="120"></textarea>
+			</view>
+			<view v-if="editorType === 'input'">
+				<input v-model="showMsg" :value="showMsg"></input>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import chatHeader from '@/components/chat-header/chat-header.vue'
+	export default {
+		name:"editorinfo",
+		data() {
+			return {
+				showMsg:''
+			};
+		},
+		props:{
+			editorType:{
+				type: String,
+				default:'textarea'
+			},
+			data:{
+				type:String,
+				default:''
+			}
+		},
+		methods:{
+			cancel(){
+				this.showMsg = this.data
+				this.$emit('cancel')
+			},
+			achivement(){
+				this.$emit('achivement',this.showMsg)
+			}
+		},
+		components:{
+			chatHeader
+		},
+		mounted(){
+			this.showMsg = this.data
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+ .editor{
+	 .header{
+		 .left{
+			 font-size: 32rpx;
+			 color: #272832;
+		 }
+		 .right{
+			 display: flex;
+			 justify-content: flex-end;
+			 width: 100%;
+			 height:100%;
+			 color:#4AAAFF;
+		 }
+	 }
+	 .edit-area{
+		 margin: 34rpx 32rpx 0 32rpx;
+		 padding: 20rpx;
+		 background-color: #F3F4F6;
+		 box-sizing: border-box;
+	 }
+ }
+</style>
--- pages/userdetail/userdetail.vue
+++ pages/userdetail/userdetail.vue
@@ -21,81 +21,98 @@
 		<view class="option">
 			<view class="left">
 				<span class="title">头像</span>
-				<image class="icon" src="../../static/images/template/02.jpg" mode="aspectFill"></image>
 			</view>
 			<view class="right">
+				<yq-avatar 
+					class="icon"
+					selWidth="200px" 
+					selHeight="400upx" 
+					@upload="myUpload" 
+					:avatarSrc="imgUrl"
+				></yq-avatar>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option">
 			<view class="left">
 				<span class="title">签名</span>
-				<view class="signture common">看没看懂阿凡达，的局啊地卡斯蒂略噩看没看懂阿凡达，的局啊地卡斯蒂略噩</view>
 			</view>
-			<view class="right">
+			<view class="right" @tap="triggerEditor('signature')">
+				<view class="signture common">{{signature}}</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option line">
 			<view class="left">
 				<span class="title">注册</span>
-				<view class="common">2022-01-15 23:33:45</view>
 			</view>
 			<view class="right">
+				<view class="common">2022-01-15 23:33:45</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
+				<!-- <picker mode="date" :value="date">
+					<view class="uni-input">{{date}}</view>
+				</picker> -->
 			</view>
 		</view>
 		<view class="option">
 			<view class="left">
 				<span class="title">昵称</span>
-				<view class="common">武动乾坤</view>
 			</view>
-			<view class="right">
+			<view class="right" @tap="editorNick">
+				<view class="common">{{nick}}</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option">
 			<view class="left">
 				<span class="title">性别</span>
-				<view class="common">男</view>
 			</view>
 			<view class="right">
+				<view class="common">
+					<picker @change="changeSex" :value="index" :range="array">
+						<view class="uni-input">{{array[index]}}</view>
+					</picker>
+				</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option">
 			<view class="left">
 				<span class="title">生日</span>
-				<view class="common">1996-12-23</view>
 			</view>
 			<view class="right">
+				<view class="common">
+					<picker mode="date" @change="changeDate" :value="time" style="height: 100%;">
+						<view class="uni-input">{{time}}</view>
+					</picker>
+				</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option">
 			<view class="left">
 				<span class="title">电话</span>
-				<view class="common">12345678901</view>
 			</view>
-			<view class="right">
+			<view class="right" @tap="editorPhone">
+				<view class="common">{{phone}}</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option line">
 			<view class="left">
 				<span class="title">邮箱</span>
-				<view class="common">123456@qq.com</view>
 			</view>
-			<view class="right">
+			<view class="right" @tap="editorEmail">
+				<view class="common">{{email}}</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
 		<view class="option line">
 			<view class="left">
 				<span class="title">密码</span>
-				<view class="common">******</view>
 			</view>
 			<view class="right">
+				<view class="common">******</view>
 				<image class="arrow-left" src="../../static/images/register/arrow-left-bold.png"></image>
 			</view>
 		</view>
@@ -108,24 +125,187 @@
 				</template>
 			</bottom-btn>
 		</view>
+		
+		<!-- 签名弹出层 -->
+		<view class="editor-wrap"  :style="{bottom:`${-clientHeight}px`}" :animation="animationSignature">
+			<editor-info @cancel="cancel('signature')" @achivement="achivementSignature" :data="signature">
+				<template v-slot:middle>
+					<view class="middle">
+						修改签名
+					</view>
+				</template>
+			</editor-info>
+		</view>
+		<!-- 昵称弹出层 -->
+		<view class="editor-wrap" :style="{bottom:`${-clientHeight}px`}" :animation="animationNick">
+			<editor-info :editorType="'input'" @cancel="cancel('nick')" @achivement="achivementNick" :data="nick">
+				<template v-slot:middle>
+					<view class="middle">
+						修改昵称
+					</view>
+				</template>
+			</editor-info>
+		</view>
+		<!-- 电话弹出层 -->
+		<view class="editor-wrap" :style="{bottom:`${-clientHeight}px`}" :animation="animationPhone">
+			<editor-info :editorType="'input'" @cancel="cancel('phone')" @achivement="achivementPhone" :data="phone">
+				<template v-slot:middle>
+					<view class="middle">
+						修改电话
+					</view>
+				</template>
+			</editor-info>
+		</view>
+		<!-- 邮箱弹出层 -->
+		<view class="editor-wrap" :style="{bottom:`${-clientHeight}px`}" :animation="animationEmail">
+			<editor-info :editorType="'input'" @cancel="cancel('email')" @achivement="achivementEmail" :data="email">
+				<template v-slot:middle>
+					<view class="middle">
+						修改邮箱
+					</view>
+				</template>
+			</editor-info>
+		</view>
 	</view>
 </template>
 
 <script>
 	import chatHeader from '../../components/chat-header/chat-header.vue'
 	import bottomBtn from '../../components/bottom-btn/bottom-btn.vue'
+	import yqAvatar from '../../components/yq-avatar/yq-avatar.vue'
+	import editorInfo from '@/components/editorinfo/editorinfo.vue'
 	export default {
 		data() {
 			return {
-				
+				imgUrl:'',
+				clientHeight:0,
+				animationSignature:{},
+				animationNick:{},
+				animationPhone:{},
+				animationEmail:{},
+				isShowSignTure:false,
+				signature:'看没看懂阿凡达，的局啊地卡斯蒂略噩看没看懂阿凡达，的局啊地卡斯蒂略噩',
+				array: ['男', '女', '未知'],
+				index:0,
+				time: '',
+				nick:'666',
+				inptValue:'',
+				phone:'',
+				email:''
 			}
 		},
+		mounted(){
+			this.getElementStyle()
+		},
 		methods: {
-			
+			getElementStyle(){
+				const query = uni.createSelectorQuery().in(this)
+				query.select('.editor-wrap').boundingClientRect(data=>{
+					this.clientHeight = data.height
+					console.log(data);
+				}).exec()
+			},
+			chooseImg(){
+				// console.log(123);
+				uni.chooseImage({
+					count:1,
+					sourceType:['album','camera'],
+					success:(res)=>{
+						console.log(res);
+						this.imgUrl = res.tempFilePaths.shift()
+					}
+				})
+			},
+			triggerEditor(signal){
+				const animationSignature = uni.createAnimation({
+				  duration: 300,
+				  timingFunction: "ease",
+				})
+				const animationNick = uni.createAnimation({
+				  duration: 300,
+				  timingFunction: "ease",
+				})
+				const animationPhone = uni.createAnimation({
+				  duration: 300,
+				  timingFunction: "ease",
+				})
+				const animationEmail = uni.createAnimation({
+				  duration: 300,
+				  timingFunction: "ease",
+				})
+				this.isShowSignTure = !this.isShowSignTure
+				if(this.isShowSignTure){
+					if(signal==='signature'){
+						animationSignature.bottom(0).step()
+					}else if(signal==='nick'){
+						animationNick.bottom(0).step()
+					}else if(signal==='phone'){
+						animationPhone.bottom(0).step()
+					}else if(signal==='email'){
+						animationEmail.bottom(0).step()
+					}
+				}else{
+					if(signal==='signature'){
+						animationSignature.bottom(-this.clientHeight).step()
+					}else if(signal==='nick'){
+						animationNick.bottom(-this.clientHeight).step()
+					}else if(signal==='phone'){
+						animationPhone.bottom(-this.clientHeight).step()
+					}else if(signal==='email'){
+						animationEmail.bottom(-this.clientHeight).step()
+					}
+				}
+				this.animationSignature = animationSignature.export()
+				this.animationNick = animationNick.export()
+				this.animationPhone = animationPhone.export()
+				this.animationEmail = animationEmail.export()
+			},
+			cancel(signal){
+				this.triggerEditor(signal)
+			},
+			achivementSignature(data){
+				this.signature = data
+				this.triggerEditor('signature')
+				// 发送请求修改
+			},
+			changeSex(data){
+				this.index = data.detail.value
+				// 发送请求修改
+			},
+			changeDate(data){
+				this.time = data.detail.value
+			},
+			editorNick(){
+				this.triggerEditor('nick')
+			},
+			achivementNick(data){
+				this.nick = data
+				this.triggerEditor('nick')
+			},
+			editorPhone(){
+				this.triggerEditor('phone')
+			},
+			achivementPhone(data){
+				this.phone = data
+				this.triggerEditor('phone')
+				//发起请求
+			},
+			editorEmail(){
+				this.triggerEditor('email')
+			},
+			achivementEmail(data){
+				this.email = data
+				this.triggerEditor('email')
+			},
+			myUpload(data){
+				this.imgUrl = data.path;
+			}
 		},
 		components:{
 			chatHeader,
-			bottomBtn
+			bottomBtn,
+			yqAvatar,
+			editorInfo
 		}
 	}
 </script>
@@ -160,10 +340,32 @@
 					font-size: 32rpx;
 					color: #272832;
 				}
+				
+			}
+			.right{
+				display: flex;
+				flex: 1;
+				justify-content: space-between;
+				align-items: center;
+				.icon{
+					width: 108rpx;
+					height: 108rpx;
+					border-radius: 20rpx;
+					margin-left: 32rpx;
+				}
+				.arrow-left{
+					transform: rotate(-180deg);
+					opacity: 0.3;
+					width: 28rpx;
+					height: 28rpx;
+				}
 				.common{
+					width: 100%;
+					height: 42rpx;
 					font-size: 32rpx;
 					color: rgba(39,40,50,0.60);
 					margin-left: 32rpx;
+					
 				}
 				.signture{
 					display: -webkit-box;
@@ -173,20 +375,6 @@
 					-webkit-line-clamp: 1;
 					-webkit-box-orient: vertical;
 					word-wrap: break-word;
-				}
-				image{
-					width: 108rpx;
-					height: 108rpx;
-					border-radius: 20rpx;
-					margin-left: 32rpx;
-				}
-			}
-			.right{
-				opacity: 0.3;
-				transform: rotate(-180deg);
-				image{
-					width: 28rpx;
-					height: 28rpx;
 				}
 			}
 		}
@@ -200,6 +388,20 @@
 			.btn-bg{
 				background-color: #ffffff;
 				color: #FF5D5B;
+			}
+		}
+		.editor-wrap{
+			position: fixed;
+			width: 100%;
+			height: 100%;
+			background-color: #fff;
+			.middle{
+				font-size: 40rpx;
+				color: #272832;
+			}
+			.right{
+				font-size: 32rpx;
+				text-align: right;
 			}
 		}
 	}
