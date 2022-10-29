75e4364804dc8a35ac4787a97a6b999853618592
peris <964480899@qq.com>
2022-10-29 23:13:16 +0800

    聊天页面 图片信息完成

--- pages/chatroom/chatroom.vue
+++ pages/chatroom/chatroom.vue
@@ -28,22 +28,29 @@
 			style="height: 100%;"
 			:show-scrollbar="false"
 			>
-				<view class="user-left user-commom">
+				<view class="user-commom" :class="msgItem.id === 'b'? 'myself-right':'user-left'"  v-for="msgItem in msgs" :key="msgItem.time">
 					<view class="msg-time">
-						11月14日 20:23
+						{{msgItem.time | timerFilter}}
 					</view>
 					<view class="icon-msg">
 						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/02.jpg"></image>
+							<image class="icon-style" :src="require(`../../static/images/template/${msgItem.imgUrl}`)" />
 						</view>
 						<view class="msg-common msg">
-							开发的，啥可大可久多久的开发
-							上课地方，是的开发绿色健康。
-							上课地方叫来，都看见。
+							<view v-if="msgItem.types === 0" class="world-break">
+								{{msgItem.message}}
+							</view>
+							<image 
+								v-if="msgItem.types === 1"
+								class="msg-img"
+								:src="require(`../../static/images/template/${msgItem.message}`)"
+								mode="widthFix"
+								@tap="previewImage(msgItem)"
+							></image>
 						</view>
 					</view>
 				</view>
-				<view class="myself-right">
+				<!-- <view class="myself-right">
 					<view class="msg-time">
 						11月15日 22:23
 					</view>
@@ -56,97 +63,7 @@
 							地方，是的开发绿色健康。
 						</view>
 					</view>
-				</view>
-				
-				<view class="user-left user-commom">
-					<view class="msg-time">
-						11月14日 20:23
-					</view>
-					<view class="icon-msg">
-						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/02.jpg"></image>
-						</view>
-						<view class="msg-common msg">
-							开发的，啥可大可久多久的开发
-							上课地方，是的开发绿色健康。
-							上课地方叫来，都看见。
-						</view>
-					</view>
-				</view>
-				<view class="myself-right">
-					<view class="msg-time">
-						11月15日 22:23
-					</view>
-					<view class="icon-msg user-commom">
-						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/01.jpg"></image>
-						</view>
-						<view class="msg msg-common">
-							的，啥可大可久多久的开发了大
-							地方，是的开发绿色健康。
-						</view>
-					</view>
-				</view>
-				
-				<view class="user-left user-commom">
-					<view class="msg-time">
-						11月14日 20:23
-					</view>
-					<view class="icon-msg">
-						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/02.jpg"></image>
-						</view>
-						<view class="msg-common msg">
-							开发的，啥可大可久多久的开发
-							上课地方，是的开发绿色健康。
-							上课地方叫来，都看见。
-						</view>
-					</view>
-				</view>
-				<view class="myself-right">
-					<view class="msg-time">
-						11月15日 22:23
-					</view>
-					<view class="icon-msg user-commom">
-						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/01.jpg"></image>
-						</view>
-						<view class="msg msg-common">
-							的，啥可大可久多久的开发了大
-							地方，是的开发绿色健康。
-						</view>
-					</view>
-				</view>
-				
-				<view class="user-left user-commom">
-					<view class="msg-time">
-						11月14日 20:23
-					</view>
-					<view class="icon-msg">
-						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/02.jpg"></image>
-						</view>
-						<view class="msg-common msg">
-							开发的，啥可大可久多久的开发
-							上课地方，是的开发绿色健康。
-							上课地方叫来，都看见。
-						</view>
-					</view>
-				</view>
-				<view class="myself-right">
-					<view class="msg-time">
-						11月15日 22:23
-					</view>
-					<view class="icon-msg user-commom">
-						<view class="icon">
-							<image class="icon-style" src="../../static/images/template/01.jpg"></image>
-						</view>
-						<view class="msg msg-common">
-							的，啥可大可久多久的开发了大
-							地方，是的开发绿色健康。
-						</view>
-					</view>
-				</view>
+				</view> -->
 			</scroll-view>
 		</view>
 	</view>
@@ -154,9 +71,83 @@
 
 <script>
 	import chatHeader from '@/components/chat-header/chat-header.vue'
+	import data from '@/mock/datas/index.js'
 	export default {
 		data() {
 			return {
+				msgs:[],
+				picUrls:[]
+			}
+		},
+		filters:{
+			timerFilter(time){
+				const oldTime = new Date(time)
+				const currentTime = new Date()
+				
+				// oldTime
+				let _timeStamp = oldTime.getTime()
+				let _year = oldTime.getFullYear()
+				let _month = oldTime.getMonth() + 1
+				let _date = oldTime.getDate()
+				let _hour = oldTime.getHours()
+				let _minutes = oldTime.getMinutes()
+				let _seconds = oldTime.getSeconds()
+				
+				// currentTime
+				let timeStamp = currentTime.getTime()
+				let year = currentTime.getFullYear()
+				let month = currentTime.getMonth() + 1
+				let date = currentTime.getDate()
+				let hour = currentTime.getHours()
+				let minutes = currentTime.getMinutes()
+				let seconds = currentTime.getSeconds()
+				
+				// 当天开始的时间戳
+				let currentPassTime =  (hour * 3600 + minutes * 60 + seconds) * 1000
+				let startTimeStamp = timeStamp - currentPassTime
+				let yesterdayStartTimeStamp = startTimeStamp - 24 * 60 * 60 * 1000
+				
+				// 同一天
+				if(_year === year && _month===month && _date===date){
+					if(_hour < 10 ){
+						_hour = `0${_hour}`
+					}
+					if(_minutes < 10){
+						_minutes = `0${_minutes}`
+					}
+					return `${_hour}:${_minutes}`
+				}
+				// 昨天
+				if(yesterdayStartTimeStamp < _timeStamp && _timeStamp < startTimeStamp){
+					if(_hour < 10 ){
+						_hour = `0${_hour}`
+					}
+					if(_minutes < 10){
+						_minutes = `0${_minutes}`
+					}
+					return `昨天${_hour}:${_minutes}`
+				}
+				// 今年
+				else if(_year === year){
+					if(_hour < 10 ){
+						_hour = `0${_hour}`
+					}
+					if(_minutes < 10){
+						_minutes = `0${_minutes}`
+					}
+					return `${_month}月${_date}日 ${_hour}:${_minutes}`
+				}else{
+					if(_hour < 10 ){
+						_hour = `0${_hour}`
+					}
+					if(_minutes < 10){
+						_minutes = `0${_minutes}`
+					}
+					return `${_year}年${_month}月${_date}日 ${_hour}:${_minutes}`
+				}
+				// 大于今年
+				
+				
 				
 			}
 		},
@@ -164,7 +155,30 @@
 			chatHeader
 		},
 		methods: {
-			
+			previewImage(msgItem){
+				const index = this.picUrls.findIndex(item=>{
+					return item === `../../static/images/template/${msgItem.message}`
+				})
+				uni.previewImage({
+					current: index,
+					urls:this.picUrls,
+					loop:true
+				})
+			},
+			getMessageList(){
+				const msgs = data.message()
+				msgs.forEach(msg=>{
+					// msg.imgUrl = `../../static/images/template/${msg.imgUrl}`
+					if(msg.types === 1){
+						this.picUrls.unshift(`../../static/images/template/${msg.message}`)
+						// msg.message = `../../static/images/template/${msg.message}`
+					}
+					this.msgs.unshift(msg)
+				})
+			}
+		},
+		mounted(){
+			this.getMessageList()
 		}
 	}
 </script>
@@ -203,11 +217,16 @@
 		padding: 8rpx 32rpx 0 32rpx;
 		scrollbar-width:none;
 		.msg-common{
-			width: 480rpx;
+			max-width: 480rpx;
 			padding: 16rpx 24rpx;
 			font-size: 32rpx;
 			color: #272832;
 			box-sizing: border-box;
+			word-wrap: break-word;
+			// line-height: 56rpx;
+			display: flex;
+			align-items: center;
+			flex-flow: row wrap;
 		}
 		.msg-time{
 			font-size: 24rpx;
@@ -231,6 +250,14 @@
 					background-color: #FFFFFF;
 					border-radius: 0 20rpx 20rpx 20rpx;
 					margin-left: 20rpx;
+					.world-break{
+						word-break: break-all;
+					}
+					.mgs-img{
+						width: 100%;
+						border-radius: 20rpx;
+						
+					}
 				}
 			}
 		}
@@ -243,6 +270,12 @@
 					background-color: #FFE431;
 					border-radius: 20rpx 0 20rpx 20rpx;
 					margin-right: 20rpx;
+					.world-break{
+						word-break: break-all;
+					}
+					.mgs-img{
+						border-radius: 20rpx;
+					}
 				}
 			}
 		}
